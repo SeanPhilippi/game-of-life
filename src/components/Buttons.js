29de1a6ab3f467f5bf37850551b255cb3f0c057a
Sean Philippi <sean.philippi@gmail.com>
2019-11-18 23:01:25 -0600

    hue color picker

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HuePicker } from 'react-color';
 
 const Buttons = ({
   color,
@@ -12,57 +13,54 @@ const Buttons = ({
   setGrid,
   generateEmptyGrid
 }) => {
+  const handleChangeComplete = color => {
+    console.log(color);
+    setColor(color.hex);
+  };
   return (
-    <div className='buttons'>
-      {/* START BUTTON */}
-      <button
-        className={running ? 'stop' : 'start'}
-        onClick={() => {
-          setRunning(!running);
-          if (!running) {
-            runningRef.current = true;
-            runSimulation();
-          }
-        }}
-      >
-        {running ? 'Stop' : 'Start'}
-      </button>
-      {/* RANDOM BUTTON */}
-      <button
-        onClick={() => {
-          const rows = [];
-          for (let i = 0; i < numRows; i++) {
-            rows.push(
-              Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0))
-            );
-          }
-          setGrid(rows);
-        }}
-      >
-        Random
-      </button>
-      {/* CLEAR BUTTON */}
-      <button
-        onClick={() => {
-          setGrid(generateEmptyGrid());
-        }}
-      >
-        Clear
-      </button>
+    <div className="settings">
+      <div className="buttons">
+        {/* START BUTTON */}
+        <button
+          className={running ? 'stop' : 'start'}
+          onClick={() => {
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+              runSimulation();
+            }
+          }}
+        >
+          {running ? 'Stop' : 'Start'}
+        </button>
+        {/* RANDOM BUTTON */}
+        <button
+          onClick={() => {
+            const rows = [];
+            for (let i = 0; i < numRows; i++) {
+              rows.push(
+                Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0))
+              );
+            }
+            setGrid(rows);
+          }}
+        >
+          Random
+        </button>
+        {/* CLEAR BUTTON */}
+        <button
+          onClick={() => {
+            setGrid(generateEmptyGrid());
+          }}
+        >
+          Clear
+        </button>
+      </div>
       {/* COLOR SELECT */}
-      <select
-        onChange={({ target: { value } }) => {
-          console.log('value', value)
-          setColor(value)
-        }}
-      >
-        <option value="red">Red</option>
-        <option value="green">Green</option>
-        <option value="blue">Blue</option>
-        <option value="purple">Purple</option>
-        <option value="orange">Orange</option>
-        Clear
-      </select>
+      <div className="hue-picker">
+        <label htmlFor="hue-picker">Hue Picker</label>
+        <HuePicker color={color} onChangeComplete={handleChangeComplete} />
+      </div>
     </div>
   );
 };
