b0e0a039e64b45e67b2c595bacbe2c980ec46d2f
Sean Philippi <sean.philippi@gmail.com>
2019-11-21 19:36:56 -0600

    oscillators

--- src/components/Guide.js
+++ src/components/Guide.js
@@ -1,36 +1,70 @@
 import React from 'react';
 import './Guide.css';
+// import Tab from './Tab';
+// import Tabs from './Tabs';
 
 const Guide = () => {
   return (
     <div className="guide text-center">
-      <div>
-        Still Lifes
-      </div>
-      <div className="diagram-row">
-        <div className="diagram diagram-block">
+      <section>
+        <div className="diagram-heading">
+          Still Lifes
         </div>
-        <div className="diagram diagram-boat">
+        <div className="diagram-row">
+          <div className="diagram diagram-block">
+          </div>
+          <div className="diagram diagram-boat">
+          </div>
         </div>
-      </div>
-      <div className="diagram-row">
-        <div className="diagram diagram-beehive">
+        <div className="diagram-row">
+          <div className="diagram diagram-beehive">
+          </div>
+          <div className="diagram diagram-loaf">
+          </div>
         </div>
-        <div className="diagram diagram-loaf">
+      </section>
+
+      <section>
+        <div className="diagram-heading">
+          Oscillators
         </div>
-      </div>
-      <div className="diagram-row">
-        <div className="diagram">
+        <div className="diagram-row">
+          <div className="diagram diagram-beacon">
+          </div>
+          <div className="diagram diagram-blinker">
+          </div>
         </div>
-        <div className="diagram">
+        <div className="diagram-row">
+          <div className="diagram diagram-penta-decathlon">
+          </div>
+          <div className="diagram diagram-pulsar">
+          </div>
         </div>
-      </div>
-      <div className="diagram-row">
-        <div className="diagram">
+        <div className="diagram-row">
+          <div className="diagram diagram-toad">
+          </div>
+          <div className="diagram">
+          </div>
         </div>
-        <div className="diagram">
+      </section>
+
+      <section>
+        <div className="diagram-heading">
+          Oscillators
         </div>
-      </div>
+        <div className="diagram-row">
+          <div className="diagram diagram-block">
+          </div>
+          <div className="diagram diagram-boat">
+          </div>
+        </div>
+        <div className="diagram-row">
+          <div className="diagram diagram-beehive">
+          </div>
+          <div className="diagram diagram-loaf">
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
---
+++ src/components/Tabs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import './Tabs.css';
+
+const Tabs = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default Tabs;
