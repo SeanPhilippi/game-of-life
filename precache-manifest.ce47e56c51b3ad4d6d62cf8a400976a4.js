a1fe7e1107ab67ba1e5b655bdf2c575ca7640406
Sean Philippi <sean.philippi@gmail.com>
2019-12-15 22:26:03 -0600

    Updates

---
+++ precache-manifest.ce47e56c51b3ad4d6d62cf8a400976a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb46506b9f8211672149572680c6b7b1",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "606eacf292bd871ae493",
+    "url": "/game-of-life/static/css/main.fa13c303.chunk.css"
+  },
+  {
+    "revision": "e241647bc37546d02873",
+    "url": "/game-of-life/static/js/2.b45cf59c.chunk.js"
+  },
+  {
+    "revision": "606eacf292bd871ae493",
+    "url": "/game-of-life/static/js/main.b4fcdd09.chunk.js"
+  },
+  {
+    "revision": "6c337d97cb58ee876942",
+    "url": "/game-of-life/static/js/runtime-main.51be29ed.js"
+  },
+  {
+    "revision": "5241cb61d23dd4ca83c8d0fb46112758",
+    "url": "/game-of-life/static/media/heavy-weight-spaceship.5241cb61.gif"
+  },
+  {
+    "revision": "661f815ca07c5c48d8508424e5e63dcc",
+    "url": "/game-of-life/static/media/light-weight-spaceship.661f815c.gif"
+  },
+  {
+    "revision": "f64b73663a4747d50034aedccfbf8f12",
+    "url": "/game-of-life/static/media/middle-weight-spaceship.f64b7366.gif"
+  },
+  {
+    "revision": "003465bddb450c93cad5b6a386e3b9ee",
+    "url": "/game-of-life/static/media/penta-decathlon.003465bd.gif"
+  }
+]);
--- src/components/Configs/Configs.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Configs = () => {
-  return <div></div>;
-};
-
-export default Configs;
